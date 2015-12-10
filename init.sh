69c3f3929b0efd8d67bd6aeb11972ac7a1bbbfde
Dennis Frank <mail@dennisfrank.info>
2015-12-10 11:22:10 +0100

    Update init script

--- init.sh
+++ init.sh
@@ -7,24 +7,36 @@ echo "=================\n"
 git clone https://github.com/dennisfrank/patternlab-scaffolding.git patternlab && \
 
 # Initialize Pattern Lab Scaffold
-cd patternlab/                                               && \
-sh init                                                      && \
-cd ..                                                        && \
+cd patternlab/ && \
+sh init        && \
+cd ..          && \
 
+# Remove no longer needed project Git repository
+rm -rf .git/       && \
+rm -rf .gitmodules && \
 
 
 echo "\n\nSetup Kirby"
 echo "===========\n"
 
+# Get Kirby Plainkit
 git clone https://github.com/getkirby/plainkit.git kirby    && \
 
 # Delete no longer needed Kirby Git files and directories
-rm kirby/.gitmodules                                        && \
-rm -rf kirby/.git                                           && \
+rm kirby/.gitmodules && \
+rm -rf kirby/.git    && \
+rm -rf kirby/kirby   && \
+rm -rf kirby/panel   && \
 
-# Delete Kirby’s empty kirby and panel directories
-# rm -rf kirby/kirby                                          && \
-# rm -rf kirby/panel
+# Create a fresh Git repository
+git init
+
+# Add Kirby components as Git submodules
+git submodule add https://github.com/getkirby/kirby.git kirby/kirby && \
+git submodule add https://github.com/getkirby/kirby.git kirby/panel && \
 
 # Get Git submodule(s)
-git submodule update --init --recursive
+git submodule update --init --recursive &&\
+
+echo "\n\nDONE!"
+echo "=====\n"
